a3eb5384243d9002fd401f95e5a7e14a1a0ba778
Li <13461898+Alachan@users.noreply.github.com>
2025-03-03 18:12:35 -0500

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: "public/build",
+    },
+    base:
+        process.env.APP_ENV === "production"
+            ? "https://cha-time.vercel.app/"
+            : "/",
 });
