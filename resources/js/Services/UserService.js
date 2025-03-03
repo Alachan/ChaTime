37dde4ae6de601e9a5b97fa113206de0a9246fa6
Li <13461898+Alachan@users.noreply.github.com>
2025-03-03 18:35:04 -0500

    some more vercel target-fix

--- resources/js/Services/UserService.js
+++ resources/js/Services/UserService.js
@@ -39,7 +39,7 @@ export default {
      * @returns {Promise} - API response with user and token
      */
     login(credentials) {
-        return axios.post(route("api.login"), credentials, {
+        return axios.post("/api/login", credentials, {
             withCredentials: true,
         });
     },
@@ -51,7 +51,7 @@ export default {
      * @returns {Promise} - API response with user and token
      */
     register(userData) {
-        return axios.post(route("api.register"), userData, {
+        return axios.post("/api/logout", userData, {
             withCredentials: true,
         });
     },
